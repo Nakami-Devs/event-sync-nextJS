c5795116a6acddb900e4663c44048236dedcb6d0
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-05-01 21:12:31 +0300

    feat(events): add PUT /api/events/{id}

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -35,3 +35,38 @@ export async function GET(_req: NextRequest, { params }: Params){
         )
     }
 }
+
+export async function PUT(req: NextRequest, { params }: Params){
+    try{
+        const { title, description, start_date, end_date, place } = await req.json()
+
+        const existing = await prisma.event.findUnique({
+            where: { id: params.id }
+        })
+
+        if(!existing){
+            return NextResponse.json(
+                { message: 'Event not found'},
+                { status: 404 }
+            )
+        }
+
+        const updated = await prisma.event.update({
+            where: { id: params.id },
+            data: {
+                ...(title && {title}),
+                ...(description && {description}),
+                ...(start_date && {start_date: new Date(start_date)}),
+                ...(end_date && {end_date: new Date(end_date)}),
+                ...(place && {place}),
+            },
+        })
+        return NextResponse.json(updated, { status: 200 })
+    } catch (error){
+        console.error('Error updating event', error)
+        return NextResponse.json(
+            { message: 'Server error' },
+            { status: 500 }
+        )
+    }
+}
