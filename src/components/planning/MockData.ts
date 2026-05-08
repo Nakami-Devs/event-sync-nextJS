612796eff3a646a8f89c46401ba539723633bf43
Koloina01 <kolomaherison@gmail.com>
2026-05-08 16:26:33 +0300

    feat: add mock data for planning sessions and rooms

---
+++ src/components/planning/MockData.ts
@@ -0,0 +1,66 @@
+export const rooms = [
+  "Salle B",
+  "Salle Pi",
+  "Salle Sigma",
+  "Salle NP",
+  "Numer"
+]
+
+export const sessions = [
+  {
+    id: 1,
+    title: "Introduction TN",
+    room: "Salle B",
+    start: "09:00",
+    end: "10:00",
+    speaker: "Jean Marc"
+  },
+  {
+    id: 2,
+    title: "Introduction IA",
+    room: "Salle Pi",
+    start: "09:00",
+    end: "10:00",
+    speaker: "Sarah Smith"
+  },
+  {
+    id: 3,
+    title: "Cyber Security",
+    room: "Salle Sigma",
+    start: "09:00",
+    end: "10:00",
+    speaker: "Alex Johnson"
+  },
+  {
+    id: 4,
+    title: "Web3 & Blockchain",
+    room: "Salle B",
+    start: "10:00",
+    end: "11:00",
+    speaker: "David Kim"
+  },
+  {
+    id: 5,
+    title: "personal branding",
+    room: "Salle NP",
+    start: "8:00",
+    end: "11:00",
+    speaker: "Dr Fortunat Andria"
+  },
+  {
+    id: 6,
+    title: "Introduction EL",
+    room: "Numer",
+    start: "13:00",
+    end: "14:00",
+    speaker: "Ryan Andriamahery"
+  },
+  {
+    id: 7,
+    title: "Introduction EL",
+    room: "Numer",
+    start: "14:00",
+    end: "16:00",
+    speaker: "Ryan Andriamahery"
+  }
+]
