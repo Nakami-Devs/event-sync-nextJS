0c80225c7f67fce14ef8a9813738bce24f547614
Koloina01 <kolomaherison@gmail.com>
2026-06-27 18:03:00 +0300

    fix: fix nabar importation for events

--- src/app/events/[id]/planning/page.tsx
+++ src/app/events/[id]/planning/page.tsx
@@ -1,5 +1,5 @@
 import PlanningGrid from "@/components/planning/PlanningGrid"
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/navbar/Navbar"
 
 export default  async function PlanningPage({params}: {params: Promise<{id: string}>}) {
   const {id} = await params;
